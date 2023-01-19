f92059f88b49347988d4f9932d7b999a94bb60a8
DatCat11 <113148989+DatCat11@users.noreply.github.com>
2023-01-19 16:52:10 -0500

    Add files via upload

---
+++ morsecoder.py
@@ -0,0 +1,19 @@
+import string
+
+chars = list(string.ascii_lowercase) + list(string.ascii_uppercase) + ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", " "]
+
+morse = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", "-.-", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."] * 2 + [".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.", "-----", "/"]
+
+morseOutput = ""
+
+print("- Welcome to Morse Coder. -\n\nThis program translates your input into Morse Code. Only letters, numbers and spaces are accepted. Any other characters will be omitted.\n")
+
+while True:
+  morseOutput = ""
+  stringInput = input("Enter your input: ")
+
+  for i in range(len(stringInput)):
+    if stringInput[i] in chars:
+      morseOutput += (morse[chars.index(stringInput[i])] + " ")
+    
+  print(f"Your output:\n\n{morseOutput}\n")
